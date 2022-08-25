f9f6083cad32fd3eb6086f138dd7534525da9a21
Joy Muthoni Malanga <joyalphamuthoni@gmail.com>
2022-08-25 11:16:39 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+const drivers=['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(drivers, attribute) {
+    return drivers.filter(function (driver) { 
+    return driver.toLowerCase() === attribute.toLowerCase() })  //using callback function to filter
+  }
+  function fuzzyMatch(drivers, attribute) {
+    return drivers.filter(function (driver) { 
+    return driver.toLowerCase().substring(0, attribute.length) === attribute.toLowerCase() })
+  }
+  function matchName(drivers,argument) {
+    return drivers.filter(function(drivers){
+    return drivers.name === argument })
+  }
